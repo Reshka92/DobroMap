a9158a50cf1f76feb50a94e2955bd859e069159b
Reshka <kirillmatveev119@gmail.com>
2025-08-31 22:07:02 +0300

    Обновление

--- index.php
+++ index.php
@@ -1,26 +1,22 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Полноэкранная карта</title>
-    <meta charset="utf-8" />
-    <link rel="stylesheet" href="css/style.css">
-    <style>
-      
-    </style>
+    <title>DobroMap - Карта добрых дел</title>
+    <meta charset="utf-8"/>
+    <link rel="stylesheet" href="css/1.css"/>
   </head>
   <body>
-    <div id="map" class="map">
-        <div class="header">
-            <header>
-                <img src="images/DAA.png" id="logo"class="logoImg" alt="">
-                <span></span>
-            </header>
-        </div>
+    <div id="map" class="map"></div>
+    
+    <!-- Контейнер с элементами управления -->
+    <div class="controls-container">
+        <button class="btnLogIn">Log In</button>
+        <img src="images/Search.png" class="SearchImg" alt="Поиск">
+        <img src="images/Graphic.png" class="SearchImg" alt="Поиск">
     </div>
-  <script src="https://api-maps.yandex.ru/2.1/?apikey=f84ab56f-6f82-4601-a010-1b6d1d69d29e&lang=ru_RU"></script>
-  <script src="js/map.js"></script>
-  <script>
-    ymaps.ready(initMap);
-  </script>
+
+
+    <script src="https://api-maps.yandex.ru/2.1/?apikey=f84ab56f-6f82-4601-a010-1b6d1d69d29e&lang=ru_RU"></script>
+    <script src="js/map.js"></script>
   </body>
 </html>
--- js/map.js
+++ js/map.js
@@ -5,6 +5,56 @@ function initMap() {
     center: center,
     zoom: 12
   });
-   map.controls.remove('trafficControl');      // Пробки
-  map.controls.remove('fullscreenControl');   // Полноэкранный режим
+  
+  // Удаляем все стандартные элементы управления
+  map.controls.remove('trafficControl');
+  map.controls.remove('fullscreenControl');
+  map.controls.remove('zoomControl');
+  map.controls.remove('rulerControl');
+  map.controls.remove('typeSelector');
+  map.controls.remove('searchControl');
+  map.controls.remove('geolocationControl');
+  
+  // Добавляем нужные элементы управления с кастомным позиционированием
+  // Кнопка слоев (вид со спутника) - левый нижний угол
+  map.controls.add('typeSelector', {
+    position: {
+      top: 'auto',
+      bottom: '40px',
+      left: '10px',
+      right: 'auto'
+    }
+  });
+  
+  // Поиск - левый нижний угол над кнопкой слоев
+  map.controls.add('searchControl', {
+    position: {
+      top: 'auto',
+      bottom: '100px',
+      left: '10px',
+      right: 'auto'
+    }
+  });
+  
+  // Масштаб - правый нижний угол
+  map.controls.add('zoomControl', {
+    position: {
+      top: 'auto',
+      bottom: '40px',
+      left: 'auto',
+      right: '10px'
+    }
+  });
+  
+  // Полноэкранный режим - правый нижний угол над масштабом
+  map.controls.add('fullscreenControl', {
+    position: {
+      top: 'auto',
+      bottom: '100px',
+      left: 'auto',
+      right: '10px'
+    }
+  });
 }
+
+ymaps.ready(initMap);
